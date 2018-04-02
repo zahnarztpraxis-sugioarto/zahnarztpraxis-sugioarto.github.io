6701e8ba064c25a532fdd293953fd0cd72560fa0
nakal <nakal@users.noreply.github.com>
2018-04-02 13:59:33 +0200

    Switch to JavaScript-base mobile device detection

--- js/email.js
+++ js/email.js
@@ -1,15 +1,12 @@
-function init()
-{
-	var s = "zahnarztpraxis";
-	var t = ".desugioarto";
-	var u = "foo"
+var s = "zahnarztpraxis";
+var t = ".desugioarto";
+var u = "foo"
 
-	u = "in" + u.replace(/o$/, "@");
-	t = u + s + "-" + t.substring(3) + t.substring(0,3);
+u = "in" + u.replace(/o$/, "@");
+t = u + s + "-" + t.substring(3) + t.substring(0,3);
 
-	var canvas = document.getElementById("insertaddy");
-	var ctx = canvas.getContext("2d");
+var canvas = document.getElementById("insertaddy");
+var ctx = canvas.getContext("2d");
 
-	ctx.font="13pt Droid Sans Mono";
-	ctx.fillText(t, 0, 17);
-}
+ctx.font="13pt Droid Sans Mono";
+ctx.fillText(t, 0, 17);
