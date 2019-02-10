c0ee2725cbbe863aaa8c2b26e5c043ec00cf3ee4
nakal <nakal@users.noreply.github.com>
2019-02-10 22:35:14 +0100

    Präzisere Notdienst-Anzeige

--- js/notdienst.js
+++ js/notdienst.js
@@ -1,9 +1,27 @@
+function notdienst_hide() {
+	notdienst.style.visibility = "hidden";
+}
+
 var notdienst = document.getElementById("timetoggle");
 if (notdienst != null) {
 	var dt = new Date();
 	var h = dt.getHours();
 	var d = dt.getDay();
-	if (d >= 1 && d <= 5 && h >= 6 && h <= 18) {
-		notdienst.style.visibility = "hidden";
+	switch (d) {
+		case 1:
+		case 2:
+		case 4:
+			if (h >= 6 && h <= 18) {
+				notdienst_hide();
+			}
+			break;
+		case 3:
+		case 5:
+			if (h >= 6 && h <= 12) {
+				notdienst_hide();
+			}
+			break;
+		default:
+			break;
 	}
 }
